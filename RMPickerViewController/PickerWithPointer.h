9ce8fdb60e1f9f31f7a8b387cd893420f063345a
Luke Sadler <luke@nineworks.co.uk>
2016-06-01 13:17:57 +0100

    customChanges

---
+++ RMPickerViewController/PickerWithPointer.h
@@ -0,0 +1,13 @@
+//
+//  PickerWithPointer.h
+//  Pods
+//
+//  Created by Luke Sadler on 01/06/2016.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PickerWithPointer : UIPickerView
+@property (nonatomic, strong) UITextField *workerTf;
+@end
